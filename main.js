035c6e803913fde30ec3dab698fa8351250b9665
Manuell00 <manuell.caselli@gmail.com>
2023-05-22 17:46:47 +0200

    Inserimento taskComplete

--- main.js
+++ main.js
@@ -2,17 +2,6 @@
 // Stampare all’interno di una lista HTML un item per ogni task.
 // Se la proprietà done è uguale a true, visualizzare il testo del task sbarrato.
 
-
-// MILESTONE 2
-// Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il task viene rimosso dalla lista.
-
-
-// MILESTONE 3
-// Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo task, che quindi viene aggiunto alla lista dei task esistenti.
-
-
-// Bonus:
-// 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il task alla lista
 // 2- cliccando sul testo dell’item, invertire il valore della proprietà done del task corrispondente (se done era uguale a false, impostare true e viceversa)
 
 const {createApp} = Vue;
@@ -51,8 +40,13 @@ createApp({
         },
 
         taskComplete(){
-            this.tasks.done = true
-            console.log(this.tasks.done);
+            if (this.tasks.done === true) {
+                this.tasks.done = false
+            }
+
+            else{
+                this.tasks.done = true
+            }
         }
     }
 }).mount("#app");
